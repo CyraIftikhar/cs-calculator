008c056527aa98d93fb63a25d345dcbd4548504a
CyraIftikhar <cyraiftikhar@hotmail.com>
2023-04-11 18:44:38 +0100

    added calculator function

--- cs-calculator/Program.cs
+++ cs-calculator/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
 
+Console.Write("Enter Operator: ");
+string op = Console.ReadLine();
+
+Console.WriteLine("Enter another number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+if (op == "+")
+{
+    Console.WriteLine(num1 + num2);
+} else if (op == "-")
+{
+    Console.WriteLine(num1 - num2);
+} else if (op == "/")
+{ Console.WriteLine(num1 / num2);
+}
+else if (op == "*")
+{ Console.WriteLine(num1 * num2);
+}
 Console.ReadLine();
